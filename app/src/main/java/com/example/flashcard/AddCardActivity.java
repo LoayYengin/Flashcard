442b309fd9462b75aaa075ca6447d432a64b55e7
LoayYengin <yengin.loay@gmail.com>
2022-03-15 17:34:51 -0500

    create new card feature

---
+++ app/src/main/java/com/example/flashcard/AddCardActivity.java
@@ -0,0 +1,43 @@
+package com.example.flashcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class AddCardActivity extends AppCompatActivity {
+    public static final String QUESTION_KEY = "question_key"; // since both MainActivity and AddCardActivity need to access the key, can declare as static variables
+    public static String ANSWER_KEY = "answer_key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        ImageView cancelButton = findViewById(R.id.cancel_imageView);
+        ImageView saveButton = findViewById(R.id.save_button_imageView);
+
+        cancelButton.setOnClickListener(view -> {
+            finish();
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent(); // this is where we will put our data
+                String inputQuestion = ((EditText) findViewById(R.id.enter_question_editText)).getText().toString(); // this gets the question text input from the editText view
+                String inputAnswer = ((EditText) findViewById(R.id.enter_answer_editText)).getText().toString(); // this gets the answer text input from the editText view
+
+                data.putExtra(QUESTION_KEY, inputQuestion); // puts a string into Intent, with the key "QUESTION_KEY"
+                data.putExtra(ANSWER_KEY, inputAnswer);
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+            }
+
+        });
+    }
+}
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.flashcard;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,19 +15,30 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     boolean isShowingAnswers = false;
 
+    TextView flashcardQuestion;
+    TextView flashcardAnswer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // get question and answer views from xml
-        TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
-        TextView flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
+        flashcardQuestion = findViewById(R.id.flashcard_question_textview);
+        flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
         TextView wrongAnswer1 = findViewById(R.id.wrong_answer1_textview);
         TextView wrongAnswer2 = findViewById(R.id.wrong_answer2_textview);
         TextView correctAnswer = findViewById(R.id.correct_answer_textview);
         ImageView toggleChoices = findViewById(R.id.toggle_choices_visibility_imageView);
 
+        ImageView addButton = findViewById(R.id.add_card_imageView);
+
+
+
+        /*
+         * Lambda function can be used instead of initializing a new View.OnClickListener() and then overriding the onCLick method
+         * similar to methods but don't need a name and can be implemented in the body of a method
+         **/
 
         // hide Question and display Answer on click
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
@@ -37,65 +50,88 @@ public class MainActivity extends AppCompatActivity {
                 flashcardAnswer.setVisibility(View.VISIBLE);
             }
         });
+        // using lambda:
+//        flashcardQuestion.setOnClickListener(view -> {
+//            Log.i("yengin", "onCLick method was clicked");
+////            Toast.makeText(MainActivity.this, "Display answer!", Toast.LENGTH_SHORT).show();
+//            flashcardQuestion.setVisibility(View.INVISIBLE);
+//            flashcardAnswer.setVisibility(View.VISIBLE);
+//        });
+        //******************************************
 
         // hide answer and display question on click
-        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("yengin", "onCLick method was clicked");
+        flashcardAnswer.setOnClickListener(view -> {
+            Log.i("yengin", "onCLick method was clicked");
 //                Toast.makeText(MainActivity.this, "Display question!", Toast.LENGTH_SHORT).show();
-                flashcardQuestion.setVisibility(View.VISIBLE);
-                flashcardAnswer.setVisibility(View.INVISIBLE);
-            }
+            flashcardQuestion.setVisibility(View.VISIBLE);
+            flashcardAnswer.setVisibility(View.INVISIBLE);
         });
 
-        wrongAnswer1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wrongAnswer1.setBackgroundColor(getResources().getColor(R.color.red_200, null));
-                wrongAnswer1.setTextColor(getResources().getColor(R.color.black));
+        wrongAnswer1.setOnClickListener(view -> {
+            wrongAnswer1.setBackgroundColor(getResources().getColor(R.color.red_200, null));
+            wrongAnswer1.setTextColor(getResources().getColor(R.color.black));
 
-                correctAnswer.setBackgroundColor(getResources().getColor(R.color.green_200, null));
-                correctAnswer.setTextColor(getResources().getColor(R.color.black));
-            }
+            correctAnswer.setBackgroundColor(getResources().getColor(R.color.green_200, null));
+            correctAnswer.setTextColor(getResources().getColor(R.color.black));
         });
 
-        wrongAnswer2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                wrongAnswer2.setBackgroundColor(getResources().getColor(R.color.red_200, null));
-                wrongAnswer2.setTextColor(getResources().getColor(R.color.black));
+        wrongAnswer2.setOnClickListener(view -> {
+            wrongAnswer2.setBackgroundColor(getResources().getColor(R.color.red_200, null));
+            wrongAnswer2.setTextColor(getResources().getColor(R.color.black));
 
-                correctAnswer.setBackgroundColor(getResources().getColor(R.color.green_200, null));
-                correctAnswer.setTextColor(getResources().getColor(R.color.black));
-            }
+            correctAnswer.setBackgroundColor(getResources().getColor(R.color.green_200, null));
+            correctAnswer.setTextColor(getResources().getColor(R.color.black));
         });
 
-        correctAnswer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                correctAnswer.setBackgroundColor(getResources().getColor(R.color.green_200, null));
-                correctAnswer.setTextColor(getResources().getColor(R.color.black));
-            }
+        correctAnswer.setOnClickListener(view -> {
+            correctAnswer.setBackgroundColor(getResources().getColor(R.color.green_200, null));
+            correctAnswer.setTextColor(getResources().getColor(R.color.black));
         });
 
-        toggleChoices.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isShowingAnswers) {
-                    toggleChoices.setImageResource(R.drawable.ic_iconmonstr_eye_8);
-                    wrongAnswer1.setVisibility(View.INVISIBLE);
-                    wrongAnswer2.setVisibility(View.INVISIBLE);
-                    correctAnswer.setVisibility(View.INVISIBLE);
-                } else {
-                    toggleChoices.setImageResource(R.drawable.ic_iconmonstr_eye_6);
-                    wrongAnswer1.setVisibility(View.VISIBLE);
-                    wrongAnswer2.setVisibility(View.VISIBLE);
-                    correctAnswer.setVisibility(View.VISIBLE);
-                }
-                isShowingAnswers = !isShowingAnswers;
+        // toggle image to display/hide multiple choice answers
+        toggleChoices.setOnClickListener(view -> {
+            if (isShowingAnswers) {
+                toggleChoices.setImageResource(R.drawable.ic_iconmonstr_eye_8);
+                wrongAnswer1.setVisibility(View.INVISIBLE);
+                wrongAnswer2.setVisibility(View.INVISIBLE);
+                correctAnswer.setVisibility(View.INVISIBLE);
+            } else {
+                toggleChoices.setImageResource(R.drawable.ic_iconmonstr_eye_6);
+                wrongAnswer1.setVisibility(View.VISIBLE);
+                wrongAnswer2.setVisibility(View.VISIBLE);
+                correctAnswer.setVisibility(View.VISIBLE);
             }
+            isShowingAnswers = !isShowingAnswers;
         });
 
+        // navigate to AddCardActivity
+        addButton.setOnClickListener(view -> {
+            Intent toAddCard = new Intent(MainActivity.this, AddCardActivity.class);
+            if (toAddCard != null) {
+//                MainActivity.this.startActivity(toAddCard);
+                startActivityForResult(toAddCard, 100); // create an activity with with the "Intent" of expecting data in return from AddCardActivity
+            }
+        });
+    }
+
+    /*
+    startActivityForResult() and onActivityResult() allows us to start another activity and receive a result back
+     If the created activity passes data back with an Intent onActivityResult() handles this data.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100) { // requestCode needs to match the 100 used in startActivityForRestult
+            // get data
+            if (data != null) { // check if there's an Intent
+                String questionString = data.getExtras().getString(AddCardActivity.QUESTION_KEY); // key String needs to match key used in the intent from AddCard
+                String answerString = data.getExtras().getString(AddCardActivity.ANSWER_KEY);
+
+                // need to set as global textview var
+                flashcardQuestion.setText(questionString);
+                flashcardAnswer.setText(answerString);
+            }
+        }
     }
 }
