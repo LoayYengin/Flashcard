510a7f878620fc25b040014b7ff40711dc3fe791
LoayYengin <yengin.loay@gmail.com>
2022-04-08 15:04:23 -0500

    complete lab4 submission

--- app/src/main/java/com/example/flashcard/AddCardActivity.java
+++ app/src/main/java/com/example/flashcard/AddCardActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
